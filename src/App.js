c55b724f860a66b6a3de52a5f118ddf54ba9122b
Thinh Phan <phan.tthinh1@gmail.com>
2021-09-27 12:25:38 -0400

    General layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Navbar, Exchanges, Cryptocurrencies, Homepage, News, CryptoDetails } from './Components';
+import './App.css';
 
 const App = () => {
     return (
-        <div>
-            <h1>Cryptoverse</h1>
+        <div className="app">
+            <div className="navbar">
+                <Navbar />
+            </div>
+            <div className="main">
+                <Layout>
+                    <div className="routes">
+                        <Switch>
+                            <Route exact path="/">
+                                <Homepage/>
+                            </Route>
+                            <Route exact path="/exchanges">
+                                <Exchanges/>
+                            </Route>
+                            <Route exact path="/cryptocurrencies">
+                                <Cryptocurrencies/>
+                            </Route>
+                            <Route exact path="/crypto/:coinId">
+                                <CryptoDetails/>
+                            </Route>
+                            <Route exact path="/news">
+                                <News/>
+                            </Route>
+                        </Switch>
+                    </div>
+                </Layout>
+            </div>
+
+            <div className="footer">
+
+            </div>
         </div>
     );
 }
